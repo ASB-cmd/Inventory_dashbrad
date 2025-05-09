a3b36b04668b4de08aa835004082b264d86cb284
Akshay Bhope <akshay.bhope@sjsu.edu>
2025-05-08 20:42:58 -0700

    Initial Commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -30,8 +30,7 @@ app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 
 // Server
-const port = process.env.PORT || 3001;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
